349c76863528ed6c6e885d4c2af0aa9dacb436f0
Arkady V. Savelev <saversavic@gmail.com>
2024-06-09 12:36:15 +0500

    add class vacancies

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,9 +1,10 @@
 class Vacancies():
 
     def __init__(self):
-        id()
-        name
-        salary_range
-        url
+        self.name = name
+        self.salary_range = salary_range
+        self.url = url
 
-
+    @classmethod
+    def add_vacancies(cls):
+        pass
